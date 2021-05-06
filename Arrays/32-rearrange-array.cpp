f89cf293f9d6c91cb363f950b1666812af31d48d
Vineet Sharma <sharmava05@gmail.com>
2021-05-06 18:50:54 +0530

    rearrange the array

---
+++ Arrays/32-rearrange-array.cpp
@@ -0,0 +1,135 @@
+#include<bits/stdc++.h>
+#include<fstream>
+#include "../commons/common.h"
+using namespace std;
+
+ifstream fin;
+void load_file_io();
+void inputArray(int* , int);
+void inputVector(vector<int>& , int);
+
+class Solution{   
+public:
+    //Function to partition the array around the range such 
+    //that array is divided into three parts.
+    void threeWayPartition(vector<int>& A,int a, int b)
+    {
+        // code here
+        int start = 0, end = A.size()-1;
+  
+        // Traverse elements from left
+        for (int i=0; i<=end;)
+        {
+            if (A[i] < a)
+                swap(A[i++], A[start++]);
+    
+            else if (A[i] > b)
+                swap(A[i], A[end--]);
+    
+            else
+                i++;
+        }        
+    }
+};
+
+int main(){
+    // comment this function to use standard I/O.
+    load_file_io();
+
+    // Use fin instead of fin.
+    // Write your code here
+    int t;
+    fin>>t;
+    while(t--)
+    {
+        int N;
+        fin>>N;
+        
+        vector<int> array(N);
+        unordered_map<int,int> ump;
+        
+        for(int i=0;i<N;i++){
+            fin>>array[i];
+            ump[array[i]]++;
+        }
+        
+        int a,b;
+        fin>>a>>b;
+        
+        vector<int> original = array;
+
+        int k1=0,k2=0,k3=0;
+        int kk1=0;int kk2=0;int kk3=0;
+        
+        for(int i=0; i<N; i++)
+        {
+            if(original[i]>b)
+                k3++;
+            else if(original[i]<=b and original[i]>=a)
+                k2++;
+            else if(original[i]<b)
+                k1++;
+        }
+        
+        Solution ob;
+        ob.threeWayPartition(array,a,b);
+        printVector(array, "");
+      
+        for(int i=0;i<k1;i++)
+        {
+            if(array[i]<b)
+            kk1++;
+        }
+        
+        for(int i=k1;i<k1+k2;i++)
+        {
+            if(array[i]<=b and array[i]>=a)
+            kk2++;
+            
+        }
+        
+        for(int i=k1+k2;i<k1+k2+k3;i++)
+        {
+            if(array[i]>b)
+            kk3++;
+        }
+        bool ok = 0;
+        if(k1==kk1 and k2 ==kk2 and k3 == kk3)
+            ok = 1;
+        
+        for(int i=0;i<array.size();i++)
+            ump[array[i]]--;
+        
+        for(int i=0;i<array.size();i++)
+            if(ump[array[i]]!=0)
+                ok=0;
+        
+        if(ok)
+            cout<<1<<endl;
+        else
+            cout<<0<<endl;
+        
+    }
+    
+    return 0;
+}
+
+
+// Common functions
+void load_file_io(){
+    fin.open("../input_output/input.txt");
+}
+
+void inputArray(int A[], int n){
+    for(int i = 0; i<n; i++){
+        fin >> A[i];
+    }
+}
+
+void inputVector(vector<int>& A, int n){
+    for(int i = 0; i<n; i++){
+        int c;
+        fin >> c;
+        A.push_back(c);
+    }
+}
